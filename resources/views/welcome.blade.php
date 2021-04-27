1f974ffeb3ceccba26d16f087d3c559ac0c666d3
alfandash <cumiasem91@gmail.com>
2021-04-28 00:00:59 +0700

    initial commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Weather App</title>
+
+    <link rel="stylesheet" href="/css/app.css">
+    <script src="/js/app.js" defer></script>
+</head>
+<body class="bg-blue-500">
+    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Repellat commodi eos deleniti. Magnam laborum doloremque earum veritatis, ipsam, nihil consequatur molestias delectus dolore consectetur culpa voluptas fugit. Vel, voluptas laborum.
+</body>
+</html>
